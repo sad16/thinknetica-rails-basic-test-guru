2704fce30bbfc187cb7f76b1793605f75f730e62
SAD <salnikov.a.d@gmail.com>
2018-12-08 21:57:34 +0300

    set timezone and locale

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module ThinkneticaRailsBasicTestguru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'St. Petersburg'
+    config.i18n.default_locale = :ru
   end
 end
