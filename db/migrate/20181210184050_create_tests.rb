195dd1b53437e5acc44e14558c8f7fb36d5dfc40
SAD <salnikov.a.d@gmail.com>
2018-12-10 21:56:25 +0300

    user model

--- db/migrate/20181210184050_create_tests.rb
+++ db/migrate/20181210184050_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[5.2]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 0
-      t.referenses :category
+      t.references :category, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_183301) do
+ActiveRecord::Schema.define(version: 2018_12_10_185534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", default: false
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
@@ -21,4 +30,29 @@ ActiveRecord::Schema.define(version: 2018_12_10_183301) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 0
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "tests", "categories"
 end
