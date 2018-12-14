8dd38d1ebc22b8c5b1c0038254f406aa88d779cd
Artem <salnikov.a.d@gmail.com>
2018-12-14 11:04:31 +0300

    add dependent options

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
-  has_many :questions
-  has_many :results
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :restrict_with_exception
   has_many :users, through: :results
 
   def self.titles(category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :results
+  has_many :results, dependent: :restrict_with_exception
   has_many :tests, through: :results
-  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :restrict_with_exception
 
   def tests_by_level(level)
     tests.where(level: level)
