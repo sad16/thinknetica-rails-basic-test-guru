8dc71e3c9ecac77ee21bb975428ad8d63094f224
Artem <salnikov.a.d@gmail.com>
2019-01-12 12:40:45 +0300

    refactor

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,11 +1,12 @@
 module TestPassagesHelper
-  def result_title(result, test_title)
-    string = result >= 85 ? 'Successfully' : 'Unsuccessfully'
+  SUCCESS_PERSENT = 85.freeze
 
+  def result_title(result, test_title)
+    string = result >= SUCCESS_PERSENT ? 'Successfully' : 'Unsuccessfully'
     "The #{test_title} Test was #{string} Completed!"
   end
 
   def result_color(result)
-    result >= 85 ? 'green' : 'red'
+    result >= SUCCESS_PERSENT ? 'green' : 'red'
   end
 end
