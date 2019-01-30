8fe272e80fa68ae0df14361349cb5562cc312905
Artem <salnikov.a.d@gmail.com>
2019-01-30 18:16:05 +0300

    add ru locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_locale
   before_action :authenticate_user!
+
+  def default_url_options
+    lang = I18n.locale != I18n.default_locale ? I18n.locale : nil
+    { lang: lang }
+  end
 
   private
 
@@ -13,5 +18,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
+  end
+
+  def set_locale
+    lang = params[:lang]
+    I18n.locale = I18n.locale_available?(lang) ? lang : I18n.default_locale
   end
 end
