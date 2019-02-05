af51541c6e40ea117d8929a005c9f7700c8937b1
Artem <salnikov.a.d@gmail.com>
2019-02-05 09:57:48 +0300

    fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,19 @@
 module ApplicationHelper
+  ALERT_TYPES = {
+    default: 'primary',
+    notice: 'success',
+    alert: 'danger'
+  }.freeze
+
   def current_year
     Time.current.year
   end
 
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
+  end
+
+  def alert_class(type)
+    ALERT_TYPES[type.to_sym] || ALERT_TYPES[:default]
   end
 end
