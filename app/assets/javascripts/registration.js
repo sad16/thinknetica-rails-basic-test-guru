f17713877875aab969d46454c88dd2037c792cc6
Artem <salnikov.a.d@gmail.com>
2019-02-07 18:33:05 +0300

    sorting test title

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,40 +1,40 @@
 document.addEventListener('turbolinks:load', function() {
-  var userPasswordConfirmation = document.querySelector('#user_password_confirmation');
+  var userPasswordConfirmation = document.querySelector('#user_password_confirmation')
 
   if (userPasswordConfirmation) {
     userPasswordConfirmation.addEventListener('input', checkPasswordConfirmation)
   }
-});
+})
 
 function checkPasswordConfirmation() {
-  var userPassword = document.querySelector('#user_password');
+  var userPassword = document.querySelector('#user_password')
 
   if (this.value.length == 0) {
-    removeValidationClasses(userPassword);
-    removeValidationClasses(this);
+    removeValidationClasses(userPassword)
+    removeValidationClasses(this)
   }
   else if (userPassword.value == this.value) {
-    addValidClass(userPassword);
-    addValidClass(this);
+    addValidClass(userPassword)
+    addValidClass(this)
   }
   else {
-    addInvalidClass(userPassword);
-    addInvalidClass(this);
+    addInvalidClass(userPassword)
+    addInvalidClass(this)
   }
+}
 
-  function removeValidationClasses(element) {
-    element.classList.remove('is-valid', 'is-invalid');
-  }
+function removeValidationClasses(element) {
+  element.classList.remove('is-valid', 'is-invalid')
+}
 
-  function addValidClass(element) {
-    removeСontainingСlass(element, 'is-invalid');
-    element.classList.add('is-valid');
-  }
+function addValidClass(element) {
+  removeСontainingСlass(element, 'is-invalid')
+  element.classList.add('is-valid')
+}
 
-  function addInvalidClass(element) {
-    removeСontainingСlass(element, 'is-valid');
-    element.classList.add('is-invalid');
-  }
+function addInvalidClass(element) {
+  removeСontainingСlass(element, 'is-valid')
+  element.classList.add('is-invalid')
 }
 
 function removeСontainingСlass(element, class_name) {
