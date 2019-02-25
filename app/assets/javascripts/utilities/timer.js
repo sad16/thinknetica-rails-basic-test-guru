d4905a539cb9b57d0e90f21c1cd3b35eaed60a66
Artem <salnikov.a.d@gmail.com>
2019-02-25 18:25:18 +0300

    finish change

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -14,14 +14,15 @@ function showTimer() {
   if (timer_seconds == 0) { submitForm() }
 
   var seconds = timer_seconds % 60;
+  var minutes = (timer_seconds - seconds) / 60;
 
-  timer_str = `${(timer_seconds - seconds) / 60}:${seconds > 9 ? seconds : `0${seconds}`}`;
-  timer.textContent = timer_str;
+  timer.textContent = `${minutes}:${seconds > 9 ? seconds : `0${seconds}`}`;
 
   timer.dataset.second = timer_seconds - 1;
 }
 
 function submitForm() {
   var form = document.querySelector('form');
+
   if (form) { form.submit() }
 }
--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -7,10 +7,4 @@ module TestPassagesHelper
   def result_color(test_passage)
     test_passage.success_result? ? 'green' : 'red'
   end
-
-  # def timer(test_passage)
-  #   return if test_passage.second_to_end == Float::INFINITY
-
-  #   I18n.t('test_passages.helpers.timer', count: (test_passage.second_to_end.to_f / 60).ceil)
-  # end
 end
